675a863124d252e237120e85bf3727b86f2cfa92
Kat Contreras <kat.contreras@gmail.com>
2022-08-17 22:58:13 -0700

    add emp inquirer

--- server.js
+++ server.js
@@ -40,3 +40,231 @@ const addDepartment = () => {
         })
 }
 
+const addRole = () => {
+    deptList = `SELECT * FROM employee_db.department`;
+    var deptChoices = [];
+    var deptJson = [];
+    db.promise().query(deptList)
+        .then(response => {
+            if (response[0].length <= 0) {
+                console.log("Please add a department to create a new role");
+            } else {
+                deptJson.push(response[0]);
+                response[0].forEach(dept => {
+                    deptChoices.push(dept.name);
+                });
+                inquirer
+                    .prompt([{
+                            type: "input",
+                            message: questions[2],
+                            name: "roleName"
+                        },
+                        {
+                            type: "input",
+                            message: questions[3],
+                            name: "salary"
+                        },
+                        {
+                            type: "list",
+                            message: questions[4],
+                            name: "department",
+                            choices: deptChoices
+                        }
+                    ]).then((response) => {
+                        var deptId;
+                        for (var i = 0; i < deptJson[0].length; i++) {
+                            if (deptJson[0][i].name == response.department) {
+                                deptId = deptJson[0][i].id;
+                            };
+                        }
+                        sql = `INSERT INTO employee_db.role(title, salary, department_id) VALUES('${response.roleName}', '${response.salary}', '${deptId}')`;
+                        queries(sql, "Role");
+                    })
+            }
+        })
+}
+
+const addEmployee = () => {
+    roles = `SELECT distinct(title),id FROM employee_db.role;`;
+    var roleList = [];
+    var roleNameList = [];
+    var managerList = [];
+    var managerNameList = ['N/A'];
+    db.promise().query(roles)
+        .then(response => {
+            if (response[0].length <= 0) {
+                console.log("Please add a role to add an employee");
+            } else {
+                roleList.push(response[0]);
+                response[0].forEach(role => {
+                    roleNameList.push(role.title);
+                });
+                managers = `SELECT * FROM employee_db.employee;`;
+                db.promise().query(managers)
+                    .then(response => {
+                        if (response[0].length > 0) {
+                            managerList.push(response[0]);
+                            response[0].forEach(manager => {
+                                managerNameList.push(`${manager.first_name} ${manager.last_name}`);
+                            });
+                        }
+                        inquirer
+                            .prompt([{
+                                    type: "input",
+                                    message: questions[5],
+                                    name: "firstName"
+                                },
+                                {
+                                    type: "input",
+                                    message: questions[6],
+                                    name: "lastName"
+                                },
+                                {
+                                    type: "list",
+                                    message: questions[7],
+                                    name: "role",
+                                    choices: roleNameList
+                                },
+                                {
+                                    type: "list",
+                                    message: questions[8],
+                                    name: "manager",
+                                    choices: managerNameList
+                                }
+                            ]).then((response) => {
+                                var roleId;
+                                var managerId;
+                                for (var i = 0; i < roleList[0].length; i++) {
+                                    if (roleList[0][i].title == response.role) {
+                                        roleId = roleList[0][i].id;
+                                    };
+                                }
+                                if (response.manager != "N/A") {
+                                    for (var i = 0; i < managerList[0].length; i++) {
+                                        if (((response.manager).includes(managerList[0][i].first_name)) && ((response.manager).includes(managerList[0][i].last_name))) {
+                                            managerId = managerList[0][i].id;
+                                        };
+                                    }
+                                }
+
+                                if (managerId) {
+                                    sql = `INSERT INTO employee_db.employee(first_name, last_name, role_id, manager_id) VALUES('${response.firstName}', '${response.lastName}', '${roleId}', '${managerId}')`;
+                                    queries(sql, "Employee");
+                                } else {
+                                    sql = `INSERT INTO employee_db.employee(first_name, last_name, role_id) VALUES('${response.firstName}', '${response.lastName}', '${roleId}')`;
+                                    queries(sql, "Employee");
+                                }
+                            })
+                    })
+            }
+        })
+}
+
+function updateEmployeeRole() {
+    var employeeList = [];
+    var employeeData = [];
+    const sql = `SELECT * FROM employee_db.employee;`;
+    db.promise().query(sql)
+        .then(response => {
+            if (response[0].length > 0) {
+                employeeData.push(response[0]);
+                response[0].forEach(employee => {
+                    employeeList.push(`${employee.first_name} ${employee.last_name}`);
+                });
+                const roleQuery = `SELECT distinct(title),id FROM employee_db.role;`;
+                var roleList = [];
+                var roleNameList = [];
+                db.promise().query(roleQuery)
+                    .then(response => {
+                        if (response[0].length <= 0) {
+                            console.log("Please add a role to add an employee");
+                        } else {
+                            roleList.push(response[0]);
+                            response[0].forEach(role => {
+                                roleNameList.push(role.title);
+                            });
+                        }
+                    })
+                inquirer
+                    .prompt([{
+                            type: "list",
+                            message: questions[11],
+                            name: "employeeName",
+                            choices: employeeList
+                        },
+                        {
+                            type: "list",
+                            message: questions[7],
+                            name: "employeeRole",
+                            choices: roleNameList
+                        }
+                    ]).then(response => {
+                        var employeeId;
+                        var roleId;
+                        for (var i = 0; i < employeeData[0].length; i++) {
+                            if (((response.employeeName).includes(employeeData[0][i].first_name)) && ((response.employeeName).includes(employeeData[0][i].last_name))) {
+                                employeeId = employeeData[0][i].id;
+                            };
+                        }
+                        for (var i = 0; i < roleList[0].length; i++) {
+                            if (roleList[0][i].title == response.employeeRole) {
+                                roleId = roleList[0][i].id;
+                            };
+                        }
+                        const sql = `UPDATE employee_db.employee SET role_id = ${roleId} WHERE id = ${employeeId}`
+                        queries(sql, "Employee");
+                    })
+            } else {
+                console.log("Please add an employee to the database!");
+            }
+        })
+
+}
+
+function updateEmployeeManager() {
+    var employeeList = [];
+    var employeeData = [];
+    const sql = `SELECT * FROM employee_db.employee;`;
+    db.promise().query(sql)
+        .then(response => {
+            if (response[0].length > 0) {
+                employeeData.push(response[0]);
+                response[0].forEach(employee => {
+                    employeeList.push(`${employee.first_name} ${employee.last_name}`);
+                });
+                inquirer
+                    .prompt([{
+                            type: "list",
+                            message: questions[10],
+                            name: "employeeName",
+                            choices: employeeList
+                        },
+                        {
+                            type: "list",
+                            message: questions[8],
+                            name: "manager",
+                            choices: employeeList
+                        }
+                    ]).then(response => {
+                        var employeeId;
+                        var managerId;
+                        for (var i = 0; i < employeeData[0].length; i++) {
+                            if (((response.employeeName).includes(employeeData[0][i].first_name)) && ((response.employeeName).includes(employeeData[0][i].last_name))) {
+                                employeeId = employeeData[0][i].id;
+                            };
+                        }
+                        for (var i = 0; i < employeeData[0].length; i++) {
+                            if (((response.manager).includes(employeeData[0][i].first_name)) && ((response.manager).includes(employeeData[0][i].last_name))) {
+                                managerId = employeeData[0][i].id;
+                            };
+                        }
+                        const sql = `UPDATE employee_db.employee SET manager_id = ${managerId} WHERE id = ${employeeId}`
+                        queries(sql, "Employee");
+                    })
+            } else {
+                console.log("Please add an employee to the database!");
+            }
+        })
+
+}
+
